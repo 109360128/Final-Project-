03da4207f3b7723f190ce8ea219733b46342eeaf
109360128 <t109360128@ntut,org.tw>
2023-01-02 21:38:00 +0800

    猜數字

---
+++ app/src/main/java/com/example/guessnumber/MainActivity1.java
@@ -0,0 +1,26 @@
+package com.example.guessnumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
